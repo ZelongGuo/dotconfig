4c6d783b2a2bed19066df718ba7a62e1d5b43004
zelong <zelong.guo@outlook.com>
2026-03-23 20:45:06 +0800

    update tmux bottom status bar

--- shared/tmux/tmux-powerline/themes/default.sh
+++ shared/tmux/tmux-powerline/themes/default.sh
@@ -41,36 +41,36 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"tmux_session_info 245 0" \
 		"hostname 247 0" \
 		"ifstat_sys 249 0" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		#"wan_ip 24 255" \
-		#"vcs_branch 29 88" \
-		#"vcs_compare 60 255" \
+		"lan_ip 24 255" \
+		# "wan_ip 24 255" \
+		# "vcs_branch 29 88" \
+		# "vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
+		# "vcs_modified 9 255" \
 		#"vcs_others 245 0" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
-		#"earthquake 3 0" \
-		#"pwd 89 211" \
+		# "earthquake 3 0" \
+		# "pwd 89 211" \
 		"load 167 15" \
         # neglect the pwd because the space is so narrow to accommodate when split the windows
-		#"pwd 1 15" \
+		# "pwd 1 15" \
 		"uptime 124 15" \
-		#"macos_notification_count 29 255" \
-		#"mailcount 9 255" \
-		#"now_playing 234 37" \
-		#"battery 137 127" \
-		#"tmux_mem_cpu_load 234 136" \
-    		"cpu 240 136" \
-		#"weather 37 255" \
-		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
-    		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		# "macos_notification_count 29 255" \
+		# "mailcount 9 255" \
+		# "now_playing 234 37" \
+		# "battery 137 127" \
+		# "tmux_mem_cpu_load 234 136" \
+    		# "cpu 240 136" \
+		# "weather 37 255" \
+		# "rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
+    		# "xkb_layout 125 117" \
+		# "date_day 235 136" \
+		# "date 235 136" \
+		# "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
