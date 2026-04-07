24acbb19d34e7cc9b04e24e56815cf4e7380ec59
zelong <zelong.guo@outlook.com>
2026-04-07 20:40:26 +0800

    move launch_dual_polybar to i3 config

--- linux/i3/scripts/monitor_hotplug.sh
+++ linux/i3/scripts/monitor_hotplug.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 set -e
 
 ~/.config/i3/scripts/monitor_setup.sh
 
 # Leave some time for arranging layout
-sleep 2
+sleep 1
 
 ~/.config/i3/scripts/workspace_init.sh
-~/.config/polybar/launch_dual_polybar.sh
 
